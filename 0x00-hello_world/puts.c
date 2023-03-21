c97690e622c02396fe9cb15bf22b86ff98b4d507
Gracious <oluwakemigrace437@gmail.com>
2023-03-20 21:31:47 -0700

    puts.c

---
+++ 0x00-hello_world/puts.c
@@ -0,0 +1,10 @@
+#include <studio.h>
+/**
+ *main - A programm that prints a line with puts function
+ *Return: 0 on success
+ */
+int main(void)
+{
+puts("\"programming is like a multilingual puzzle");
+return (0);
+}
